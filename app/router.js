0b400a9172f574f9ecb53f242c412361e9924f59
pifen <pifen.ellwood@jpmchase.com>
2015-04-30 17:55:07 -0400

    'initial'

--- app/router.js
+++ app/router.js
@@ -2,10 +2,14 @@ import Ember from 'ember';
 import config from './config/environment';
 
 var Router = Ember.Router.extend({
-  location: config.locationType
+    location: config.locationType
 });
 
 export default Router.map(function() {
-  this.resource('documents', { path: '/' });
+    this.resource('documents', { path: '/' });
+
+    this.resource("administration",  function() {
+        this.route("adduser");
+ });
 
 });
